390ab53eca2e2190c06877d06c71cc5a09345e6e
floriansr <sueurfloriansf@gmail.com>
2020-06-08 17:49:46 +0200

    versioning, controller users, sanitizer devise, postwoman ok

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::UsersController < Api::ApiController
+  before_action :authenticate_user!, except: %i[show index]
+  before_action :set_user, only: %i[show update destroy]
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    render json: @user
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:username)
+    end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+	before_action :sanitize_devise_params, if: :devise_controller?
+	include ActionController::MimeResponds
+	respond_to :json
+
+	  def sanitize_devise_params
+	    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+	  end
 end
